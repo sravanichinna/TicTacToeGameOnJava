b00f56a329b0d10382afb0e63e99ce02a4ad4fcc
sravanichinna <sravanidane413.com>
2020-12-03 10:56:14 +0530

    Added: TicTacToe UC1 logic

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,5 +1,18 @@
 public class TicTacToeGame{
 	public static char[] gameBoard() {
 		System.out.println("Welcome to TicTacToe game");
+		char[] board =new char[10];
+		for(int i=1;i<10;i++)
+		{
+			board[i]='-';
+			System.out.println("-");
+			
+		
+		}
+		return board;
+	}
+	public static void main(String[] args)
+	{
+		char[] board=gameBoard();
 	}
 }
