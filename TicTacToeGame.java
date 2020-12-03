c3ac0021ae1cd272fe5fc0f2ffa63e1d3060b4a4
sravanichinna <sravanidane413.com>
2020-12-03 11:39:31 +0530

    Added: Player Enter X or O logic

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,18 +1,42 @@
-public class TicTacToeGame{
+import java.util.Scanner;
+
+public class TicTacToeGame {
+	public static String Player;
+	public static String Computer;
+	
 	public static char[] gameBoard() {
-		System.out.println("Welcome to TicTacToe game");
 		char[] board =new char[10];
 		for(int i=1;i<10;i++)
 		{
-			board[i]='-';
-			System.out.println("-");
-			
-		
+			board[i]=' ';
+			System.out.println("");
 		}
 		return board;
+	}
+	
+//UC2
+	public static String inputBoard()
+	{
+		String Player;
+		String Computer;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Choose Between O or X");
+		Computer=scanner.next();
+		if(Computer=="X")
+		{
+			Player="O";
+		}
+		else
+		{
+			Player="X";
+		}
+		return Computer;
 	}
 	public static void main(String[] args)
 	{
 		char[] board=gameBoard();
+		String input=inputBoard();
 	}
+
 }
+
